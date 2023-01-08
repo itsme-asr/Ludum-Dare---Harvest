dde496b8ad25e0e0b51529c0f6ebb4bb3fdbb125
A.S.Rawat <adityasrwt5@gmail.com>
2023-01-08 21:58:24 +0530

    Harvest

--- Harvest/Assets/Script/Spawner.cs
+++ Harvest/Assets/Script/Spawner.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject toSpawn;
-    [SerializeField] private int numberofSpawn = 4;
+    [SerializeField] private float time = 1f;
+    private float currentTime;
     void Start()
     {
-        Clone();
+
+    }
+
+    private void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            fruitSpawn();
+            currentTime = time;
+
+        }
 
     }
 
 
-    private void Clone()
+    private void fruitSpawn()
     {
         Vector3 position = new Vector3(Random.Range(-7.77f, 8f), Random.Range(3.74f, 0.29f), Random.Range(0f, 0f));
-        Instantiate(toSpawn, position, Quaternion.identity);
+        GameObject fruit = Instantiate(toSpawn, position, Quaternion.identity);
+        Destroy(fruit, 5f);
     }
+
 }
